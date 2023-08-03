4feae859323f48047ead4d4d3181c424270ec383
Anneassuncao05 <104541181+Anneassuncao05@users.noreply.github.com>
2023-08-03 10:16:42 -0300

    Add files via upload

---
+++ lib/model/categorias.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../data/mock_data.dart';
+
+class Categoria{
+    final String id;
+    final String titulo;
+    final Color cores;
+    Categoria({@required this.id, @required this.titulo, @required this.cores});
+}
