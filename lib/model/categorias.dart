ebf21499d000852ba6d1fb597bee01f3cd7ac572
Anneassuncao05 <104541181+Anneassuncao05@users.noreply.github.com>
2023-08-31 09:49:31 -0300

    Delete lib directory
    Signed-off-by: Anneassuncao05 <104541181+Anneassuncao05@users.noreply.github.com>

--- lib/model/categorias.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../data/mock_data.dart';
-
-class Categoria{
-    final String id;
-    final String titulo;
-    final Color cores;
-    Categoria({@required this.id, @required this.titulo, @required this.cores});
-}
